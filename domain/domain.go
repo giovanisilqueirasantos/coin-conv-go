6f901ec34e9065de0e1150b1800c8f5312c51cf9
Giovani Silqueira Santos <giovani.silqueira.santos@gmail.com>
2023-07-21 13:24:38 -0300

    feat: implement amaunt in domain

--- domain/domain.go
+++ domain/domain.go
@@ -1,6 +1,11 @@
 package domain
 
-import "errors"
+import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 type currency struct {
 	Text   string
@@ -28,4 +33,41 @@ func NewCurrency(text string) (*currency, error) {
 	}
 
 	return cur, nil
+}
+
+type amount float64
+
+func NewAmount(text string) (*amount, error) {
+	if text == "" {
+		return nil, errors.New("text can not be empty")
+	} else if strings.Contains(text, ".") {
+		if !regexp.MustCompile(`^(?:[0-9])[0-9]*\.(?:[0-9])[0-9]*$`).MatchString(text) {
+			return nil, errors.New("text in worng format n.n")
+		}
+	} else {
+		if !regexp.MustCompile(`^[0-9]*$`).MatchString(text) {
+			return nil, errors.New("text can only contain numbers")
+		}
+	}
+
+	textSplited := strings.Split(text, ".")
+
+	if len(textSplited) > 1 {
+		if len(textSplited[1]) > 2 {
+			textSplited[1] = textSplited[1][0:2]
+		}
+	}
+
+	textFloat, err := strconv.ParseFloat(strings.Join(textSplited, "."), 64)
+	if err != nil {
+		return nil, err
+	}
+
+	am := amount(textFloat)
+	return &am, nil
+}
+
+func ConvertCurrency(to currency, amountQuant amount, rate amount) (*amount, string) {
+	am := amount(rate * amountQuant)
+	return &am, to.Symbol
 }
--- domain/domain_test.go
+++ domain/domain_test.go
@@ -10,4 +10,28 @@ func TestConvertCurrency(t *testing.T) {
 	real, err := NewCurrency("real")
 	assert.Nil(t, err)
 	assert.NotNil(t, real)
+
+	dollar, err := NewCurrency("dollar")
+	assert.Nil(t, err)
+	assert.NotNil(t, dollar)
+
+	realAmount, err := NewAmount("10")
+	assert.Nil(t, err)
+	assert.NotNil(t, realAmount)
+
+	dollarAmount, err := NewAmount("10")
+	assert.Nil(t, err)
+	assert.NotNil(t, dollarAmount)
+
+	dollarRate, err := NewAmount("4.50")
+	assert.Nil(t, err)
+	assert.NotNil(t, dollarRate)
+
+	realAmountConverted, currencySymbol := ConvertCurrency(*real, *dollarAmount, *dollarRate)
+	assert.Equal(t, amount(45), *realAmountConverted)
+	assert.Equal(t, "R$", currencySymbol)
+
+	dolarAmountConverted, currencySymbol := ConvertCurrency(*dollar, *realAmount, *dollarRate)
+	assert.Equal(t, amount(45), *realAmountConverted)
+	assert.Equal(t, "$", currencySymbol)
 }
